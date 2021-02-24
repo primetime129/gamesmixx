d99e3d3ceed1221378be9c5c11c97c741d1c64be
primetime129 <79603802+primetime129@users.noreply.github.com>
2021-02-24 21:55:09 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
+client.login(process.env.BOT_TOKEN);
